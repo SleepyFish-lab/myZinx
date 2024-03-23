8f8de84c2b5e653c560d4f9f85794fefcc053510
interesting <1265657403@qq.com>
2024-03-23 15:11:40 +0800

    zinx v0.2

--- zinx/znet/server.go
+++ zinx/znet/server.go
@@ -1,6 +1,7 @@
 package znet
 
 import (
+	"errors"
 	"fmt"
 	"myZinx/zinx/ziface"
 	"net"
@@ -16,6 +17,17 @@ type Server struct {
 	IP string
 	//服务器的端口
 	Port int
+}
+
+// 定义当前客户端连接所绑定的handle api （目前这个handle是写死的，以后优化应该由用户自定义）
+func CallBackToClient(conn *net.TCPConn, data []byte, n int) error {
+	//回显的业务
+	fmt.Println("[Conn Handel] CallBackToClient ......")
+	if _, err := conn.Write(data[:n]); err != nil {
+		fmt.Println("Write back buf err", err)
+		return errors.New("CallBackToClient error")
+	}
+	return nil
 }
 
 func (s *Server) Start() {
@@ -36,6 +48,8 @@ func (s *Server) Start() {
 		}
 		fmt.Println("start Zinx server succ,", s.Name, "succ, listenning....")
 
+		var cid uint32 = 0
+
 		//3.阻塞的等待客户端的连接，处理客户端的连接业务（读写）
 		for {
 			//如果有客户的连接，阻塞会返回
@@ -46,24 +60,14 @@ func (s *Server) Start() {
 				//即继续接收其他的连接
 				continue
 			}
-			//已经与客户端建立连接，做一些业务：做一个基本的512字节的内容回显
-			go func() {
-				for {
-					buf := make([]byte, 512)
-					n, err := conn.Read(buf)
-					if err != nil {
-						fmt.Println("recv buf error", err)
-						continue
-					}
-					fmt.Printf("recv buf %s,cnt=%d ", buf, n)
-					//回显功能
-					if _, err := conn.Write(buf[:n]); err != nil {
-						fmt.Println("write buf back err", err)
-						continue
-					}
-					fmt.Println()
-				}
-			}()
+			//解耦合，使用connection模块的handler处理
+			//将处理业务的方法与conn进行绑定，得到我们的连接模块
+			dealConn := NewConnection(conn, cid, CallBackToClient)
+			cid++
+
+			//启动当前的连接业务处理
+			go dealConn.Start()
+
 		}
 	}()
 
